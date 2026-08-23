779233138e197a181d04175055c036d8fd7354f8
felsokning <github@felsokning.se>
2026-08-23 03:45:28 +0100

    Fixing MsBuild Call (Again)

--- Get-WaitChainAnalysis.ps1
+++ Get-WaitChainAnalysis.ps1
@@ -26,7 +26,7 @@ else {
         if (Test-Path -Path "C:$($directorySeparator)Program Files$($directorySeparator)Microsoft Visual Studio$($directorySeparator)18$($directorySeparator)$($flavour)$($directorySeparator)Common7$($directorySeparator)Tools$($directorySeparator)VsDevCmd.bat") {
             Write-Host -ForegroundColor Green "Found VsDevCmd.bat for $($flavour)"
             if ($x64) {
-                $buildCppJob = Start-Job -ScriptBlock { "C:$($using:directorySeparator)Program Files$($using:directorySeparator)Microsoft Visual Studio$($using:directorySeparator)18$($using:directorySeparator)$($using:flavour)$($using:directorySeparator)Common7$($using:directorySeparator)Tools$($using:directorySeparator)VsDevCmd.bat" && msbuild "$($using:PWD)$($using:directorySeparator)src$($using:directorySeparator)cpp$($using:directorySeparator)UnmanagedDebugging.vcxproj" $($using:directorySeparator)p:configuration=release $($using:directorySeparator)p:platform=x64 }
+                $buildCppJob = Start-Job -ScriptBlock { "C:$($using:directorySeparator)Program Files$($using:directorySeparator)Microsoft Visual Studio$($using:directorySeparator)18$($using:directorySeparator)$($using:flavour)$($using:directorySeparator)Common7$($using:directorySeparator)Tools$($using:directorySeparator)VsDevCmd.bat" && msbuild.exe "$($using:PWD)$($using:directorySeparator)src$($using:directorySeparator)cpp$($using:directorySeparator)UnmanagedDebugging.vcxproj" $($using:directorySeparator)p:configuration=release $($using:directorySeparator)p:platform=x64 }
                 # Wait for the build job to finish
                 while($buildCppJob.State -ne "Completed" -and $buildCppJob.State -ne "Failed") {
                     Write-Host "Waiting 5 seconds for the build job to complete... State: $($buildCppJob.State)"
@@ -48,14 +48,14 @@ else {
             }
             elseif ($x86) {
                 Write-Host -ForegroundColor Green "Found VsDevCmd.bat for $($flavour)"
-                $buildCppJob = Start-Job -ScriptBlock { "C:$($using:directorySeparator)Program Files$($using:directorySeparator)Microsoft Visual Studio$($using:directorySeparator)18$($using:directorySeparator)$($using:flavour)$($using:directorySeparator)Common7$($using:directorySeparator)Tools$($using:directorySeparator)VsDevCmd.bat" && msbuild "$($using:PWD)$($using:directorySeparator)src$($using:directorySeparator)cpp$($using:directorySeparator)UnmanagedDebugging.vcxproj" $($using:directorySeparator)p:configuration=release $($using:directorySeparator)p:platform=x86 }
+                $buildCppJob = Start-Job -ScriptBlock { "C:$($using:directorySeparator)Program Files$($using:directorySeparator)Microsoft Visual Studio$($using:directorySeparator)18$($using:directorySeparator)$($using:flavour)$($using:directorySeparator)Common7$($using:directorySeparator)Tools$($using:directorySeparator)VsDevCmd.bat" && msbuild.exe "$($using:PWD)$($using:directorySeparator)src$($using:directorySeparator)cpp$($using:directorySeparator)UnmanagedDebugging.vcxproj" $($using:directorySeparator)p:configuration=release $($using:directorySeparator)p:platform=x86 }
                 # Wait for the build job to finish
-                while($buildJob.State -ne "Completed" -and $buildJob.State -ne "Failed") {
-                    Write-Host "Waiting 5 seconds for the build job to complete... State: $($buildJob.State)"
+                while($buildCppJob.State -ne "Completed" -and $buildCppJob.State -ne "Failed") {
+                    Write-Host "Waiting 5 seconds for the build job to complete... State: $($buildCppJob.State)"
                     Start-Sleep -Seconds 5
                 }
 
-                if ($buildJob.State -eq "Failed") {
+                if ($buildCppJob.State -eq "Failed") {
                     Write-Error -Message "Build job failed. Please check the build logs for details."
                     break;
                 }
