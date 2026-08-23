e5cc35a5c4c84fa3561c1eec029de061d9209b0e
felsokning <github@felsokning.se>
2026-08-23 01:21:16 +0100

    Reverting DLL Import Path Changes

--- Get-WaitChainAnalysis.ps1
+++ Get-WaitChainAnalysis.ps1
@@ -43,9 +43,9 @@ else {
             }
             elseif ($x86) {
                 Write-Host -ForegroundColor Green "Found VsDevCmd.bat for $($flavour)"
-                $buildJob = $(Start-Job -ScriptBlock { "C:$($directorySeparator)Program Files$($directorySeparator)Microsoft Visual Studio$($directorySeparator)18$($directorySeparator)$($flavour)$($directorySeparator)Common7$($directorySeparator)Tools$($directorySeparator)VsDevCmd.bat"; msbuild "$($PWD)$($directorySeparator)src$($directorySeparator)cpp$($directorySeparator)UnmanagedDebugging.vcxproj" $($directorySeparator)p:configuration=release $($directorySeparator)p:platform=x86 })
+                $buildJob = Start-Job -ScriptBlock { "C:$($using:directorySeparator)Program Files$($using:directorySeparator)Microsoft Visual Studio$($using:directorySeparator)18$($using:directorySeparator)$($using:flavour)$($using:directorySeparator)Common7$($using:directorySeparator)Tools$($using:directorySeparator)VsDevCmd.bat"; msbuild "$($using:PWD)$($using:directorySeparator)src$($using:directorySeparator)cpp$($using:directorySeparator)UnmanagedDebugging.vcxproj" $($using:directorySeparator)p:configuration=release $($using:directorySeparator)p:platform=x86 }
                 # Wait for the build job to finish
-                while($($buildJob.State -ne "Completed") -and $($buildJob.State -ne "Failed")) {
+                while($buildJob.State -ne "Completed" -and $buildJob.State -ne "Failed") {
                     Write-Host "Waiting 5 seconds for the build job to complete... State: $($buildJob.State)"
                     Start-Sleep -Seconds 5
                 }
@@ -74,7 +74,7 @@ $Source = @"
 
         public static class Debug
         {
-            [DllImport("$($AssemblyPath)")]
+            [DllImport($($AssemblyPath))]
             public static extern IntPtr ExternalEntry(int id);
 
             public static string GetThreadWaitChain(int id)
